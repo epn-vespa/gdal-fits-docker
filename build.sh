5a07aaf316848e19aa9fb831384a287453f77f01
Chiara Marmo <cmarmo@users.noreply.github.com>
2018-09-19 17:30:19 +0200

    First try for FITS GDAL installing

--- build.sh
+++ build.sh
@@ -4,54 +4,58 @@
 # Install GDAL from within a docker container
 #
 # This script is designed to be run from within a docker container in order to
-# install GDAL. It delegates to `before_install.sh` and `install.sh` which are
-# patched from the Travis CI configuration in the GDAL repository.
+# install GDAL.
 #
 
 set -e
 
 DIR=$(dirname "$(readlink -f "$0")")
-GDAL_VERSION=$(cat ${DIR}/gdal-checkout.txt)
+#GDAL_VERSION=$(cat ${DIR}/gdal-checkout.txt)
 
-export DEBIAN_FRONTEND=noninteractive
+#export DEBIAN_FRONTEND=noninteractive
 
 # Set the locale. Required for subversion to work on the repository.
-update-locale LANG="C.UTF-8"
-dpkg-reconfigure locales
-. /etc/default/locale
-export LANG
+#update-locale LANG="C.UTF-8"
+#dpkg-reconfigure locales
+#. /etc/default/locale
+#export LANG
 
-# Instell prerequisites.
-apt-get update -y
-apt-get install -y \
-        software-properties-common \
-        wget \
+# Install prerequisites.
+dnf install -y \
+#        software-properties-common \
+#        wget \
         unzip \
-        subversion \
-        ccache \
-        clang-3.5 \
-        patch \
-        python-dev \
-        ant
+#        subversion \
+        ccache-devel \
+        clang-devel \
+#        patch \
+        python3-devel \
+        cfitsio-devel \
+        automake \
+        autoconf
 
 # Everything happens under here.
 cd /tmp
 
 # Get GDAL.
-svn checkout --quiet "http://svn.osgeo.org/gdal/${GDAL_VERSION}/" /tmp/gdal/
+git clone https://github.com/epn-vespa/gdal.git .
 
 # Install GDAL.
-cd /tmp/gdal
+cd /tmp/gdal/gdal
+sh autogen.sh
+make -j
+make install
 
 # Apply our build patches.
-patch ./gdal/ci/travis/trusty_clang/before_install.sh ${DIR}/before_install.sh.patch
-patch ./gdal/ci/travis/trusty_clang/install.sh ${DIR}/install.sh.patch
+#patch ./gdal/ci/travis/trusty_clang/before_install.sh ${DIR}/before_install.sh.patch
+#patch ./gdal/ci/travis/trusty_clang/install.sh ${DIR}/install.sh.patch
 
 # Do the build.
-. ./gdal/ci/travis/trusty_clang/before_install.sh
-. ./gdal/ci/travis/trusty_clang/install.sh
+#. ./gdal/ci/travis/trusty_clang/before_install.sh
+#. ./gdal/ci/travis/trusty_clang/install.sh
+
 
 # Clean up.
-apt-get autoremove -y
-apt-get clean
-rm -rf /var/lib/apt/lists/partial/* /tmp/* /var/tmp/*
+#apt-get autoremove -y
+#apt-get clean
+#rm -rf /var/lib/apt/lists/partial/* /tmp/* /var/tmp/*
