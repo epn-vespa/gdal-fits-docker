86a2ef211b071221326ce001bc4a98bc8d932d44
Chiara Marmo <chiara.marmo@u-psud.fr>
2018-09-25 10:32:45 +0200

    Successful build.

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@
 # Install GDAL from within a docker container
 #
 # This script is designed to be run from within a docker container in order to
-# install GDAL. It delegates to `before_install.sh` and `install.sh` which are
-# patched from the Travis CI configuration in the GDAL repository.
+# install GDAL.
 #
 
 set -e
@@ -32,9 +31,10 @@ apt-get install -y \
         automake \
         autoconf \
         make \
-        python-dev \
-        ant
-        #python3-astropy python3-numpy python3-matplotlib jupyter-notebook
+        python3-dev \
+        ant \
+        python3-astropy python3-numpy python3-matplotlib #\
+        #jupyter-notebook
 
 # everything happens under here.
 cd /usr/local/src/gdal-fits-docker/
@@ -42,9 +42,9 @@ cd /usr/local/src/gdal-fits-docker/
 # Install GDAL.
 cd gdal/gdal
 ./autogen.sh
-sh configure # that was missing see [linux - config.status not found - Super User](https://superuser.com/a/272116)
-make -j
-make install
+./configure
+make #-s
+make -s install
 
 # Compile python bindings
 cd swig/python
@@ -52,19 +52,10 @@ python3 setup.py build
 python3 setup.py install
 
 # Linking libraries in standardpath (because python is looking there)
-cd /usr/lib64
-ln -s /usr/local/lib64/libgdal* .
-
-# Apply our build patches.
-#patch ./gdal/ci/travis/trusty_clang/before_install.sh ${DIR}/before_install.sh.patch
-#patch ./gdal/ci/travis/trusty_clang/install.sh ${DIR}/install.sh.patch
-
-# Do the build.
-#. ./gdal/ci/travis/trusty_clang/before_install.sh
-#. ./gdal/ci/travis/trusty_clang/install.sh
-
+cd /usr/lib
+ln -s /usr/local/lib/libgdal* .
 
 # Clean up.
-#apt-get autoremove -y
-#apt-get clean
-#rm -rf /var/lib/apt/lists/partial/* /tmp/* /var/tmp/*
+apt-get autoremove -y
+apt-get clean
+rm -rf /var/lib/apt/lists/partial/* /tmp/* /var/tmp/*
